ab8bdfcb5070f4974e5e34d0f12385718d5b02e7
andile-ux <jwayelephi01@gmail.com>
2023-11-07 13:05:27 +0200

    marker

---
+++ marker.js
@@ -0,0 +1,75 @@
+let result = 90;
+let distinction = result>=85;
+let excemption = result >= 75 && result < 85;
+let achieved = result>= 50 && result < 75;
+let notAchieved = result < 50 && result >= 0;
+let didNotWrite = 0;
+
+
+function pn()
+{
+    if(result>0)
+    {
+        if(distinction)
+        {
+            console.log("distinction");
+
+        }
+        if(excemption)
+        {
+            console.log("excemption");
+
+        }
+        if(achieved)
+        {
+            console.log("achieved");
+
+        }
+        if(notAchieved)
+        {
+            console.log("not achieved");
+
+        }
+        
+    }
+    else
+    {
+        console.log("did not write");
+    };
+}
+
+function switching() 
+{
+    
+    switch (result)
+        {
+        case  distinction :
+            console.log("distinction");
+            break;
+
+        case excemption:
+            console.log("exemption");
+            break;
+
+        case achieved:
+            console.log("achieved");
+            break;
+
+        case notAchieved:
+            console.log("not achieved");
+            break;
+
+        default:
+            
+            console.log("did not write");
+    }
+   
+}
+
+// function calls
+pn();
+switching();
+
+
+
+
